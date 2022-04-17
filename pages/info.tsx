c9dfa4d7c0baffe97d03e87ff39658f00a24c090
Mitchell 'Zyrn' Lee <mitch@zyrn.dev>
2022-04-17 16:09:32 +1000

    Added Build Time

--- pages/info.tsx
+++ pages/info.tsx
@@ -6,6 +6,10 @@ import TagLine from '@/components/tagline';
 import { Link } from '@components/core';
 import { NextPage, GetStaticProps } from 'next';
 import env from '@/lib/env';
+import { Code } from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
+import { GridItem } from '@chakra-ui/react';
+import { FC } from 'react';
 
 const meta: MetaOptions = {
   title: "Information",
@@ -16,19 +20,52 @@ export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       version: env.version,
+      time: (new Date()).getTime()
     },
   };
 }
 
-const Info: NextPage<{ version: string }> = ({ version }) => {
+type BuildStats = {
+  version: string;
+  time: number;
+}
+
+const Stats: FC<{ stats: string[][]}> = ({ stats }) => {
+  return (
+    <Grid templateColumns="repeat(2, 1fr)">
+      {stats.map(([key, value]) => (
+        <Stat name={key} value={value} />
+      ))}
+    </Grid>
+  );
+}
+
+const Stat: FC<{name: string, value: string}> = ({ name, value }) => {
+  return (
+    <>
+      <GridItem>
+        <TagLine as="h2" size="md" textAlign="center" m="0">
+          {name}:
+        </TagLine>
+      </GridItem>
+      <GridItem>
+        <Code size="lg">{value}</Code>
+      </GridItem>
+    </>
+  );
+}
+
+const Info: NextPage<BuildStats> = (buildStats) => {
   return (
     <Layout meta={meta}>
       <Heading as="h1" size="4xl" m="0.83rem 0">
         Information
       </Heading>
-      <TagLine as="h2" size="lg">
-        Current Version = &apos;{version}&apos;
-      </TagLine>
+      <Stats stats={[
+        ["Build Commit", buildStats.version],
+        ["Build Time", (new Date(buildStats.time)).toLocaleString("en-AU", {timeZone: "Australia/Sydney"}) + " (AEST)"],
+
+      ]} />
     </Layout>
   )
 }
